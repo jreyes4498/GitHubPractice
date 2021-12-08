057169ff6b78086720c63172807c4b65e0e78e79
John Reyes <jreyes4498@stu.d214.org>
2021-12-08 10:46:17 -0600

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by John Reyes on 12/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
